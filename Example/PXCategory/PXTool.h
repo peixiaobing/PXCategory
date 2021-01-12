2aeb7e3f286efd61a7f393bb373884cf6cfa9c7b
john <john@guojiang.tv>
2021-01-12 18:15:14 +0800

    3.0

---
+++ Example/PXCategory/PXTool.h
@@ -0,0 +1,17 @@
+//
+//  PXTool.h
+//  PXCategory_Example
+//
+//  Created by john on 2021/1/12.
+//  Copyright © 2021 john. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PXTool : NSObject
++ (void) tool;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/PXCategory/PXTool.m
@@ -0,0 +1,15 @@
+//
+//  PXTool.m
+//  PXCategory_Example
+//
+//  Created by john on 2021/1/12.
+//  Copyright © 2021 john. All rights reserved.
+//
+
+#import "PXTool.h"
+
+@implementation PXTool
++ (void)tool {
+    NSLog(@"PXTool tool");
+}
+@end
